e235bcf50fe9fdc247e0641fef212cf5b8079eab
amrocha <andre.rocha@shopify.com>
2019-09-20 12:44:03 -0700

    Post comment

--- index.ts
+++ index.ts
@@ -15,14 +15,33 @@ async function main() {
 
     const files = requestRawData.data.map((datum) => datum.filename);
 
+    console.log('============================================');
+    console.log(
+      'These are the files for which the splash zone is being calculated:',
+    );
     console.log(files);
+    console.log('============================================');
 
     const dependencies = await getDependencies(
       getInput('codebaseGlob'),
       getInput('ignoreGlob'),
       files,
     );
+
+    console.log('============================================');
+    console.log('These are the dependencies calculated:');
     console.log(dependencies);
+    console.log('============================================');
+    console.log('Posting comment to github...');
+
+    await octokit.issues.createComment({
+      owner: context.payload.pull_request.base.repo.owner.login,
+      repo: context.payload.pull_request.base.repo.name,
+      issue_number: context.payload.number, // eslint-disable-line babel/camelcase
+      body: JSON.stringify(dependencies, undefined, 2),
+    });
+
+    console.log('Done!');
   }
 }
 
